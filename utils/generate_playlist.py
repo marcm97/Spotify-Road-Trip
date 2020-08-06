a547025610c8fe3a82bbd7e4719b38ddc02216b8
Shradha <shashwatishradha5@gmail.com>
2020-08-06 15:17:23 -0600

    breaking from duration while loop

--- utils/generate_playlist.py
+++ utils/generate_playlist.py
@@ -33,11 +33,12 @@ def add_songs(playListID, songs, duration, sp, username):
     # Filter from Spotify playlists?
     # Look into sliders? radar charts?
     song_set = set()
-
+    count=0
     while duration > 0:  # TODO: add condition if duration < 0 (hack: stop early if duration doesn't change)
         k = 4  # bias towards more popular songs by taking popularity to k power
         bag = songs.sample(weights=[int(x)**k if str(x).isdigit() else 0 for x in songs.popularity])
-
+        if count>10:
+            duration=0
         if bag.iloc[0, -5] not in song_set:
             song_set.add(bag.iloc[0, -5])
             uri = bag.iloc[0, -4]  # TODO: change to column name later
@@ -48,13 +49,17 @@ def add_songs(playListID, songs, duration, sp, username):
             except:
                 pass
 
+        else:
+            count+=1
 
 def make_roadtrip_playlist(origin, destination, playlist_id, sp, username):
     duration = trip_duration_seconds(origin, destination) * 1000 
     states = list(states_along_route(origin, destination).keys())  # TODO: weight towards destination state/cities
     count = len(states)
+    print(states)
     for state in states: #TODO: Handle songs that are not available in spoyify
         songs = pd.read_csv(os.path.join(os.path.dirname(__file__), "../final_datasets/" + state + ".csv"))  # TODO: read from github instead of local
         songs = songs[songs["uris"] != "error"]
         songs = songs[songs["popularity"] != "0"]
+        print("**********" + state)
         add_songs(playlist_id, songs, duration / count, sp, username)
